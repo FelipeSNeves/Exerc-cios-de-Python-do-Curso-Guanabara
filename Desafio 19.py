821d452a497dc8b179bc8b5c1daf8fc2de080ea1
Felipe Neves <164694464+FelipeSNeves@users.noreply.github.com>
2024-04-20 17:22:08 -0300

    Delete Desafio 19.py

--- Desafio 19.py
+++
@@ -1,8 +0,0 @@
-import random
-a1 = str(input('Digite o primeiro maconheiro:'))
-a2 = str(input('O segundo:'))
-a3 = str(input('O terceiro:'))
-a4 = str(input('O quarto:'))
-list = [a1 , a2 , a3 , a4]
-escolhido = random.choice(list)
-print('O que vai botar o seu hoje é o:{}'.format(escolhido))
